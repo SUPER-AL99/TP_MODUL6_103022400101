97cc5c98f37126c2ed8582103cde7765561dd111
Salman Al Farizin <salfarisi778@gmail.com>
2026-04-06 01:25:37 +0700

    menambahkan implementasi design by contract

--- TP_MODUL6_103022400101/Program.cs
+++ TP_MODUL6_103022400101/Program.cs
@@ -1,1 +1,72 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Diagnostics;
+
+namespace TP_MODUL6_103022400101
+{
+    public class SayaMusicTrack
+    {
+        private int id;
+        private int playCount;
+        private string title;
+
+        public SayaMusicTrack(string title)
+        {
+            Debug.Assert(title != null, "Error: Judul track tidak boleh null.");
+            Debug.Assert(title.Length <= 100, "Error: Judul track maksimal 100 karakter.");
+
+            this.title = title;
+            this.playCount = 0;
+
+            Random rnd = new Random();
+            this.id = rnd.Next(10000, 100000);
+        }
+
+        public void IncreasePlayCount(int count)
+        {
+            Debug.Assert(count <= 10000000, "Error: Input penambahan play count maksimal 10.000.000 per pemanggilan.");
+
+            try
+            {
+                checked
+                {
+                    this.playCount += count;
+                }
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Exception Terjadi: Proses penambahan play count gagal karena melebihi batas maksimum integer (Overflow).");
+            }
+        }
+
+        public void PrintTrackDetails()
+        {
+            Console.WriteLine($"ID Track    : {this.id}");
+            Console.WriteLine($"Judul Lagu  : {this.title}");
+            Console.WriteLine($"Play Count  : {this.playCount}");
+            Console.WriteLine("--------------------------------------------------");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=== PENGUJIAN NORMAL ===");
+            SayaMusicTrack track1 = new SayaMusicTrack("Lagu Perdana");
+            track1.PrintTrackDetails();
+
+            track1.IncreasePlayCount(5000000);
+            track1.PrintTrackDetails();
+
+
+            Console.WriteLine("\n=== PENGUJIAN EXCEPTION (OVERFLOW) ===");
+            SayaMusicTrack track2 = new SayaMusicTrack("Lagu Viral");
+
+            for (int i = 0; i < 250; i++)
+            {
+                track2.IncreasePlayCount(10000000);
+            }
+            track2.PrintTrackDetails();
+        }
+    }
+}
--- TP_MODUL6_103022400101/obj/Debug/net10.0/TP_MODUL6_103022400101.AssemblyInfo.cs
+++ TP_MODUL6_103022400101/obj/Debug/net10.0/TP_MODUL6_103022400101.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TP_MODUL6_103022400101")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03b98de3b824a115e82c39fcffca7560b3adee9d")]
 [assembly: System.Reflection.AssemblyProductAttribute("TP_MODUL6_103022400101")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TP_MODUL6_103022400101")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
